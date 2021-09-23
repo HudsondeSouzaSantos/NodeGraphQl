af9a389530f37f86fc5a1cbcd3e3f75b35810b4d
Hudson de Souza Santos <hudson_586@yahoo.com.br>
2021-09-23 00:57:38 -0300

    #4 Schema & Query  Type Scalar Quey  Resolvers

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
-const { gql , ApolloServer } = require( "apollo-server" )
+const { gql , ApolloServer } = require( "apollo-server" );
+
+/**
+ * Scalar Types
+ * - Int
+ * - Float
+ * - String
+ * - Boolean
+ * - ID
+ */
+
+ const typeDefs = gql`
+ type Query {
+     idade : Int
+     salario: Float
+     nome: String
+     ativo: Boolean
+     id: ID
+ }
+`;
 
 const resolvers = {
     Query: {
-        hello(){
-            return 'World';
+        idade() {
+            return 18;
+        },
+        salario() {
+            return 1234.56;
+        },
+        nome() {
+            return 'Graph QL';
+        },
+        ativo() {
+            return true;
+        },
+        id() {
+            return 123456789;
         }
     }
-}
-
-const typeDefs = gql`
-    type Query {
-        hello: String
-    }
-`
+};
 
 const server = new ApolloServer( {
     typeDefs,
     resolvers
-} )
+} );
 
-server.listen()
+server.listen();
